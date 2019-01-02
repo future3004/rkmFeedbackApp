db20e75484f2e565d9aa887cda55c9c2e6cf3cce
richa <richardmel@outlook.com>
2019-01-02 11:03:20 -0600

    changed greeting

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const app = express();
 
+// app url: https://frozen-bayou-13794.herokuapp.com/
+
 app.get("/", (req, res) => {
-  res.send({ hi: "Richard KM!" });
+  res.send({ hi: "Richard KM! What can i do for you today?" });
 });
 
 const PORT = process.env.PORT || 5000;
